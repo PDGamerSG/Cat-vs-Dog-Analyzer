491c0436dbe26a347db7f019432a9ef8c18ec270
PDGamerSG <pallab4586das@gmail.com>
2025-08-01 12:23:30 +0530

    modal issue fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,24 @@
 import os
 from dotenv import load_dotenv
 import streamlit as st
-from app1 import classify_image_with_scores
+from modal import Function
 load_dotenv()
+app = "cat-dog-app"
+func = "classify_image"
+classify = Function.from_name(app, func)
 st.set_page_config(page_title="Cat vs Dog Classifier", layout="centered")
 st.title("Cat vs Dog Classifier")
 uploaded = st.file_uploader("Upload a cat or dog photo", type=["png", "jpg", "jpeg"])
 if uploaded:
     st.image(uploaded, use_container_width=True, caption="Your upload")
     img_bytes = uploaded.read()
-
     with st.spinner("Identifying…"):
-        result = classify_image_with_scores(img_bytes)
+        invoc = classify.remote(img_bytes)
+        result = invoc.result()
     scores = result["all_scores"]
     cats_score = scores["cats"]
     dogs_score = scores["dogs"]
     st.write(f"🐱 Cat: {cats_score:}%   🐶 Dog: {dogs_score:}%")
-
     if result["predicted_label"] == "cats":
         st.success("🐱 It's a CAT :)")
     else:
